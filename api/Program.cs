543635270e6b7f4e74344a3cc4bc220408bc88c9
Tommy <tommycrabtree@outlook.com>
2025-12-12 23:27:27 -0500

    Added PwaCorsPolicy

--- api/Program.cs
+++ api/Program.cs
@@ -2,6 +2,13 @@ using api.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var allowedOrigins = new[]
+{
+    "http://localhost:4200",
+    "https://localhost:4200",
+    "https://decpwa.web.app",
+    "https://decpwa.firebaseapp.com"
+};
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -9,10 +16,21 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PwaCorsPolicy", policy =>
+    {
+        policy
+            .WithOrigins(allowedOrigins)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("PwaCorsPolicy");
 app.MapControllers();
 
 app.Run();
