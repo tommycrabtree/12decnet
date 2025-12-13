baf4b46bec352e11de86794e0a38270188535a72
Tommy <tommycrabtree@outlook.com>
2025-12-12 21:40:23 -0500

    End of Section 2

---
+++ api/Controllers/MembersController.cs
@@ -0,0 +1,29 @@
+using api.Data;
+using api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MembersController(AppDbContext context) : ControllerBase
+{
+    [HttpGet] // localhost:5001/api/members
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    {
+        var members = await context.Users.ToListAsync();
+
+        return members;
+    }
+
+    [HttpGet("{id}")] // localhost:5001/api/members/rebel-id
+    public async Task<ActionResult<AppUser>> GetMember(string id)
+    {
+        var member = await context.Users.FindAsync(id);
+
+        if (member == null) return NotFound();
+
+        return member;
+    }
+}
