d56f8e6810b5e9fa209b17abe7ba595b11493a2e
Tommy Crabtree <tommycrabtree@outlook.com>
2026-07-25 18:51:17 -0700

    FreezerUnits migration

---
+++ api/Data/Migrations/20260726014907_FreezerUnits.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FreezerUnits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FreezerUnits",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FreezerUnits",
+                table: "Products");
+        }
+    }
+}
